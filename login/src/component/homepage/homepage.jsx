4255a1fa61441a59bfc87868c618d289d8c0dbc3
yashmaurya321-crypto <yashmaurya2109.gmail.com>
2023-11-02 19:05:56 +0530

    login project

---
+++ login/src/component/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.css'
+const Homepage = () =>{
+return(
+    <div className='Homepage'>
+        <h1>Hello welcome to Homepage</h1>
+        
+    </div>
+)
+}
+export default Homepage;
+
